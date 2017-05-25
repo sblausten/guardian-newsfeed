81fe14743d8f28045865533face3dac03ca15619
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-05-25 15:38:27 +0100

    wrote first failing test for displaying headlines w/ mock

---
+++ spec/features/displayHeadlinesSpec.js
@@ -0,0 +1,34 @@
+(function() {
+  'use strict';
+
+  require ('../framework.js');  
+
+  describe("displays headlines");
+    function loadHeadlineDoubles() {
+      return ["Headline 1", "Headline 2", "Headline 3", "Headline 4", "Headline 5"]
+    }
+    window.location = 'http://localhost:8080/';
+    loadHeadlinesDoubles(); 
+    //our code
+    isEqual('displays correct number of headlines on page',
+            document.getElementsByTagName('li').length, 5);
+  endDescribe();
+
+
+  // describe("displays headlines");
+  //   window.location = 'http://localhost:8080/';
+  //   //mock the api
+  //   document.getElementsByTagName('li')[0].click();
+  //   isEqual('content of headlines matches that of Guardian website',
+  //           document.getElementsByTagName('ul'));
+  // endDescribe();
+
+
+  // describe("viewing a note's full text");
+  //   document.getElementById('back-to-all-notes').click();
+  //   isEqual('back-to-notes button returns user to index page',
+  //           'http://localhost:8080/',
+  //           window.location.href);
+  // endDescribe();
+
+}());
