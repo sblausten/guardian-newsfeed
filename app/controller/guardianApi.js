cee6128a9b0b1fda4d91a884f1ea32084398eed8
Alex Chalk <alexchalk17@gmail.com>
2017-05-26 11:11:14 -0400

    Article titles and summaries render successfully

--- app/controller/guardianApi.js
+++ app/controller/guardianApi.js
@@ -1,4 +1,3 @@
-
 var apiRequestUrl =
 "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/uk-news&from-date=2017-05-26?show-fields=header";
 
@@ -6,12 +5,29 @@ var articleList = new Articles();
 requestAPI(apiRequestUrl);
 
 function requestAPI() {
+  var articleSummary;
   var xhr = new XMLHttpRequest();
   xhr.open("GET", apiRequestUrl, false);
   xhr.send();
-  var array = JSON.parse(xhr.response).response.results;
-  var length = array.length;
+  guardianArticles = JSON.parse(xhr.response).response.results;
+  var length = guardianArticles.length;
   for (var i = 0; i < length; i++) {
-  	articleList.saveArticle(array[i].webTitle);
-	};
+    console.log(guardianArticles[i]);
+    var address = 'http://www.theguardian.com/' + guardianArticles[i].id;
+    document.getElementById('headlines').insertAdjacentHTML('afterbegin', '<li id="' + i + '"><a href=' + address + '>' + guardianArticles[i].webTitle + '</a><br></li>');
+    requestSummary(address);
+    document.getElementById(i).insertAdjacentHTML('beforeend', requestSummary(address));
+  }
 }
+function requestSummary(address) {
+  var apiRequestUrl = 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=' + address;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", apiRequestUrl, false);
+  xhr.send();
+  return (JSON.parse(xhr.response).sentences.join(" "));
+}
+//  var xhr = new XMLHttpRequest();
+ // xhr.open("GET", 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=http://www.theguardian.com/' + guardianArticles[i].id, false);
+
+//  xhr.send();
+//  JSON.parse(xhr.response).response.results;
--- app/view/render.js
+++ app/view/render.js
@@ -1,28 +1,29 @@
-
 (function() {
 
-	var printHeadline = function(index) {
-	    insertLi(createLi(getText(index)));
-	}
+  var printHeadline = function(index) {
+    insertLi(createLi(getText(index)));
+  };
 
-	var getText = function(index) {
-	    return document.createTextNode(headlinesArray[index]);
-	}
+  var getText = function(index) {
+    return document.createTextNode(guardianArticleTitles[index]);
+  };
 
-	var createLi = function(text) {
-		var li = document.createElement('li');
-		li.appendChild(text);
-	    return li;
-	}
+  var createLi = function(text) {
+    var li = document.createElement('li');
+    li.appendChild(text);
+    return li;
+  };
 
-	var insertLi = function(li) {
-		document
-	        .getElementById('headlines')
-	        .appendChild(li);
-	}
+  var insertLi = function(li) {
+    document
+      .getElementById('headlines')
+      .appendChild(li);
+  };
 
-	for (var i = 0; i < headlinesArray.length; i++) {
-    	printHeadline(i);
-	}
+  var guardianArticleTitles;
+  requestAPI(apiRequestUrl);
+  for (var i = 0; i < guardianArticleTitles.length; i++) {
+    printHeadline(i);
+  }
 
 })();
